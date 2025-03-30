a9c3c195d69cb9966dccb591dc1d13c04b4e5435
Figo.Xu <xujh945@qq.com>
2025-03-30 22:21:20 +0800

    调整导航效果

--- case/todo/client/src/components/Navigation.tsx
+++ case/todo/client/src/components/Navigation.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import { Box, Flex, Link, Spacer } from '@chakra-ui/react';
-import { Link as RouterLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const Navigation: React.FC = () => {
+  const linkStyle = {
+    fontSize: 'lg',
+    fontWeight: 'medium',
+    _hover: { textDecoration: 'none' }
+  };
+
+  const activeStyle = {
+    color: 'blue.500',
+    fontWeight: 'bold'
+  };
+
   return (
     <Box bg="gray.100" px={4} py={4}>
       <Flex maxW="container.md" mx="auto">
-        <Link as={RouterLink} to="/" fontSize="lg" fontWeight="bold" mr={6}>
+        <Link
+          as={NavLink}
+          to="/"
+          {...linkStyle}
+          _activeLink={activeStyle}
+          end
+          mr={6}
+        >
           本地待办事项
         </Link>
-        <Link as={RouterLink} to="/remote" mr={6}>
+        <Link
+          as={NavLink}
+          to="/remote"
+          {...linkStyle}
+          _activeLink={activeStyle}
+          mr={6}
+        >
           远程待办事项
         </Link>
         <Spacer />
-        <Link as={RouterLink} to="/about">
+        <Link
+          as={NavLink}
+          to="/about"
+          {...linkStyle}
+          _activeLink={activeStyle}
+        >
           关于
         </Link>
       </Flex>
