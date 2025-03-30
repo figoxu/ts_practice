fcefad4ae3575d4ca2c2a749517b1fdf3d8d0906
Figo.Xu <xujh945@qq.com>
2025-03-30 10:14:48 +0800

    minor

--- case/react-todo/src/App.tsx
+++ case/react-todo/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Figo Xu IsAwesome
         </a>
       </header>
     </div>
