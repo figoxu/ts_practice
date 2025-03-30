da8669b3d6a3b0ed22147cfb5214f9d8c05d85b2
Figo.Xu <xujh945@qq.com>
2025-03-30 10:31:32 +0800

    add route for reactjs

--- case/react-todo/src/App.tsx
+++ case/react-todo/src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Todo from './pages/Todo';
+import About from './pages/About';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Figo Xu IsAwesome
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Todo />} />
+            <Route path="/todo" element={<Todo />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
