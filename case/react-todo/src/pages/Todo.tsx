a5c5bd9d6dedbfd299d619b9b267237a2583793e
Figo.Xu <xujh945@qq.com>
2025-03-30 11:09:49 +0800

    minor todo

--- case/react-todo/src/pages/Todo.tsx
+++ case/react-todo/src/pages/Todo.tsx
@@ -1,13 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Todo.css';
 
+interface TodoItem {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
 const Todo: React.FC = () => {
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          text: newTodo.trim(),
+          completed: false,
+        },
+      ]);
+      setNewTodo('');
+    }
+  };
+
+  const handleDeleteTodo = (id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const handleToggleTodo = (id: number) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      handleAddTodo();
+    }
+  };
+
   return (
     <div className="todo-container">
       <h1>待办事项列表</h1>
       <div className="todo-content">
-        <p>这里将是待办事项的列表内容</p>
-        <button className="add-todo-btn">添加新任务</button>
+        <div className="todo-input">
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            onKeyPress={handleKeyPress}
+            placeholder="请输入新的待办事项"
+          />
+          <button onClick={handleAddTodo}>添加</button>
+        </div>
+        <ul className="todo-list">
+          {todos.map(todo => (
+            <li key={todo.id} className={todo.completed ? 'completed' : ''}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleToggleTodo(todo.id)}
+              />
+              <span>{todo.text}</span>
+              <button onClick={() => handleDeleteTodo(todo.id)}>删除</button>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
